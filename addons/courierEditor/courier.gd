46cdc6dcdf63c6449b34bb7c88919b576a9b1490
Justin <awenonian@gmail.com>
2023-06-18 16:40:37 -0400

    Attempted fix at courier initialization

--- addons/courierEditor/courier.gd
+++ addons/courierEditor/courier.gd
@@ -4,7 +4,7 @@ extends Path2D
 
 @export var sprite: Texture2D:
 	set(value):
-		var spriteNode: Sprite2D = self.get_node("spriteFollow/personSprite");
+		var spriteNode: Sprite2D = get_node("spriteFollow/personSprite");
 		spriteNode.texture = value;
 		sprite = value;
 		#notify_property_list_changed()
@@ -12,14 +12,13 @@ extends Path2D
 @export_range(0, 1, .01) var progress_ratio: float:
 	set(value):
 		progress_ratio = value;
-		self.get_node("spriteFollow").progress_ratio = value;
+		get_node("spriteFollow").progress_ratio = value;
 		#notify_property_list_changed()
 
 @export_color_no_alpha var nodeColor: Color = Color.RED
 
 @export var speedScale = 1;
 
-var needsInstantiate = true
 var roadmap: RoadMap
 var requiredNodes: Array[Intersection] = []
 var totalNodes: Array[Intersection] = []
@@ -27,29 +26,15 @@ var totalNodes: Array[Intersection] = []
 const PATH_WIGGLE = 0
 
 # Called when the node enters the scene tree for the first time.
-func _enter_tree():
+func _ready():
 	self.add_to_group('courier', true)
 	self.roadmap = get_tree().get_first_node_in_group('roadmap')
-	
-	#move self to child of roadmap
-	if self.get_parent() != self.roadmap and self.get_parent() != null and self.roadmap != null:
-		get_parent().remove_child(self)
-		self.roadmap.add_child(self)
-		
-	# var courierScene = preload("res://addons/courierEditor/courier.tscn")
-	# var courier = courierScene.instantiate()
-	# courier.needsInstantiate = false
-	# self.roadmap.add_child.call_deferred(courier)
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("readycallback")
 	self.progress_ratio = self.progress_ratio;
 
 func _process(delta):
 	if !Engine.is_editor_hint():
 		self.progress_ratio += speedScale * 0.05 * delta;
+		pass
 
 func _draw():
 	if Engine.is_editor_hint():
@@ -83,7 +68,7 @@ func redoPath(roadmap: RoadMap):
 		path.append_array(roadmap.findPath(requiredNodes[-1], requiredNodes[0]))
 		self.curve.clear_points()
 		for n in path:
-			self.curve.add_point(self.roadmap.to_local(n.position+Vector2(PATH_WIGGLE*randf(), PATH_WIGGLE*randf())))
+			self.curve.add_point(roadmap.to_local(n.position+Vector2(PATH_WIGGLE*randf(), PATH_WIGGLE*randf())))
 			
 		totalNodes = path
 	else:
--- addons/courierEditor/courierInit.gd
+++ addons/courierEditor/courierInit.gd
@@ -1,9 +1,12 @@
 @tool
+class_name CourierInit
 extends Node2D
 
 # Called when the node enters the scene tree for the first time.
-func _enter_tree():
-	var courierScene = preload("res://addons/courierEditor/courier.tscn")
-	var courier = courierScene.instantiate()
-	self.replace_by(courier)
-
+func _ready():
+	var courierScene = load("res://addons/courierEditor/courier.tscn")
+	# TODO: Not sure about this 1, if problems,
+	var courier = courierScene.instantiate(1)
+	get_parent().add_child(courier)
+	courier.owner = get_parent()
+	self.queue_free()
