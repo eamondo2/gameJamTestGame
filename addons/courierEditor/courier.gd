406975daf737d9810c7ce7ff4a13ce40ca524ffe
Justin Patzer <awenonian@gmail.com>
2023-06-20 15:34:29 -0400

    Added ingame controls to courier

--- addons/courierEditor/courier.gd
+++ addons/courierEditor/courier.gd
@@ -15,15 +15,21 @@ extends Node2D
 		get_node("Path/spriteFollow").progress_ratio = value;
 		#notify_property_list_changed()
 
-@export_color_no_alpha var nodeColor: Color = Color.RED
+@export_color_no_alpha var color: Color = Color.RED
 
 @export var speedScale = 1;
 
 var roadmap: RoadMap
 var requiredNodes: Array[Intersection] = []
 var totalNodes: Array[Intersection] = []
+var dragging = false
+var selected = false
+var dropTarget
+var target
 
-const PATH_WIGGLE = 0
+const PATH_WIGGLE = 20
+const INTERACTION_DISTANCE = 10
+const DROP_DISTANCE = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,14 +40,37 @@ func _ready():
 func _process(delta):
 	if !Engine.is_editor_hint():
 		self.progress_ratio += speedScale * 0.05 * delta;
+		if dropTarget != null and (dropTarget.position - position).length() < DROP_DISTANCE:
+			dropTarget = null
+			# Drop the package
 		
 func _input(event):
-	pass
+	if !Engine.is_editor_hint():
+		if event is InputEventMouseButton:
+			if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+				if (event.position - position).length() < INTERACTION_DISTANCE:
+					dragging = true
+				else:
+					dragging = false
+			else:
+				if dragging:
+					var node = roadmap.selectNode(event.position)
+					if node is DropOff:
+						dropTarget = node.position
+				dragging = false
+		elif event is InputEventMouseMotion and dragging:
+			target = event.position
+			queue_redraw()
 
 func _draw():
 	if Engine.is_editor_hint():
 		for n in requiredNodes:
-			draw_circle(n.position, 7, nodeColor)
+			draw_circle(n.position, 7, color)
+	else:
+		if dragging and target != null:
+			draw_line(position, target, color, 3)
+		if dropTarget != null:
+			draw_circle(dropTarget, 7, color)
 
 func addNode(node: Intersection):
 	if requiredNodes.find(node) == -1:
@@ -74,7 +103,7 @@ func redoPath(roadmap: RoadMap):
 		path.append_array(roadmap.findPath(requiredNodes[-1], requiredNodes[0]))
 		curveObj.curve.clear_points()
 		for n in path:
-			curveObj.curve.add_point(roadmap.to_local(n.position+Vector2(PATH_WIGGLE*randf(), PATH_WIGGLE*randf())))
+			curveObj.curve.add_point(roadmap.to_local(n.position+Vector2(PATH_WIGGLE*(randf()-.5), PATH_WIGGLE*(randf()-.5))))
 			
 		totalNodes = path
 	else:
