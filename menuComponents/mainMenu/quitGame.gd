a638ac9d7948b9483cd75f21abca1485af12acf3
eamondo2 <eamonbb@gmail.com>
2023-07-09 13:57:33 -0400

    revert camera tweaks add quit button to main menu

--- menuComponents/mainMenu/quitGame.gd
+++ menuComponents/mainMenu/quitGame.gd
@@ -1,17 +1,16 @@
-extends Camera2D
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(get_parent().size)
-	print(self.get_viewport().size)
-	print(self.zoom)
-	self.zoom =  (Vector2(self.get_viewport().size) / Vector2(get_parent().size))
-	print(self.zoom)
-	print(self.get_viewport().size)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
